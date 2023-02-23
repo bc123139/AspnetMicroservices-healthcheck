34a58e496e4734a4f10ba3fb566a225fbc8b803d
Usman Tahir <Usman.tahir@synergy-it.com>
2023-02-23 23:16:46 +0500

    Install and Configure SeriLog For ElasticSearch and Kibana Sink Integration

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using Serilog.Sinks.Elasticsearch;
 
 namespace AspnetRunBasics
 {
@@ -12,6 +15,22 @@ namespace AspnetRunBasics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration.Enrich.FromLogContext().Enrich.WithMachineName().WriteTo.Console()
+                    .WriteTo.Elasticsearch
+                    (new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+                    {
+                        
+                            IndexFormat = $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                            AutoRegisterTemplate = true,
+                            NumberOfShards = 2,
+                            NumberOfReplicas = 1
+                        
+                    }).Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                    .ReadFrom.Configuration(context.Configuration);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
